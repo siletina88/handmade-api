b743afae8740ee2af3ed65de911efef4b22f6bf6
Sasa Mehmedagic <sasamehmedagic@gmail.com>
2021-10-08 20:45:37 +0200

    test

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-const userRoute = require('./routes/user')
-const authRoute = require('./routes/auth')
-const productRoute = require('./routes/product')
-const cartRoute = require('./routes/cart')
-const orderRoute = require('./routes/order')
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
 
-dotenv.config()
+dotenv.config();
+app.use(cors());
 
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
-    console.log('DB Connection Successful')
+    console.log("DB Connection Successful");
   })
   .catch((err) => {
-    console.log(err)
-  })
+    console.log(err);
+  });
 
-app.use(express.json())
-app.use('/api/users', userRoute)
-app.use('/api/auth', authRoute)
-app.use('/api/products', productRoute)
-app.use('/api/cart', cartRoute)
-app.use('/api/orders', orderRoute)
+app.use(cors());
+app.use(express.json());
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/products", productRoute);
+app.use("/api/cart", cartRoute);
+app.use("/api/orders", orderRoute);
 
 app.listen(process.env.PORT || 5000, () => {
-  console.log('Backend Server is running')
-})
+  console.log("Backend Server is running");
+});
