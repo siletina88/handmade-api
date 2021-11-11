ba02c40ae55fb95fdf4f8a64443350f0607ad5a8
Sasa Mehmedagic <sasamehmedagic@gmail.com>
2021-11-11 15:02:03 +0100

    change

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -160,7 +160,7 @@ exports.signup = async (req, res) => {
       <body style="background-color: #a9a9a9; margin: 0 !important; padding: 0 !important">
         <!-- HIDDEN PREHEADER TEXT -->
         <div style="display: none; font-size: 1px; color: #fefefe; line-height: 1px; font-family: 'Lato', Helvetica, Arial, sans-serif; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden">
-          We're thrilled to have you here! Get ready to dive into your new account.
+          Dobrodošli na našu webshop aplikaciju!
         </div>
         <table border="0" cellpadding="0" cellspacing="0" width="100%">
           <!-- LOGO -->
